658e2a4f5da8963bdcf4bd3276d168d5e6638add
Adham-Amin <adhm0amin@gmail.com>
2025-03-24 05:15:39 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MiniStore());
+}
+class MiniStore extends StatelessWidget {
+  const MiniStore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
